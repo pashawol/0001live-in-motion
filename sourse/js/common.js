a3d5e347fe59ee7cca8c8c333771c17817b61334
Pavel <wol1414@gmail.com>
2019-08-13 13:17:59 +0300

    add zoom

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -1,6 +1,6 @@
 var $ = jQuery;
 var btnToggle = $(".toggle-menu-mobile--js"),
-		menu = $(".menu-mobile--js")
+	menu = $(".menu-mobile--js")
 
 jQuery(document).ready(function ($) {
 	// полифил для object-fit
@@ -8,19 +8,19 @@ jQuery(document).ready(function ($) {
 	// Picture element HTML5 shiv
 	document.createElement("picture");
 	// для свг
-	svg4everybody({}); 
+	svg4everybody({});
 	JSCCommon.magnificPopupCall();
 
 	JSCCommon.tabscostume('tabs');
 
 	JSCCommon.mobileMenu();
 
-	JSCCommon.inputMask(); 
-	JSCCommon.moreLine(); 
-	JSCCommon.zhowZoomIng(); 
- 
+	JSCCommon.inputMask();
+	JSCCommon.moreLine();
+	JSCCommon.zhowZoomIng();
 
-	$(".prod-head__group-title--js").click(function(){
+
+	$(".prod-head__group-title--js").click(function () {
 		$(this).next().slideToggle();
 	})
 	// добавляет подложку для pixel perfect
@@ -28,7 +28,7 @@ jQuery(document).ready(function ($) {
 	// /добавляет подложку для pixel perfect
 
 
- 
+
 
 	function heightses() {
 
@@ -42,8 +42,8 @@ jQuery(document).ready(function ($) {
 		// скрывает моб меню
 		// }
 		// var topH = 	$('.top-nav  ').innerHeight();
-		function navFixed(){
-			var topH = 	$('header').height();
+		function navFixed() {
+			var topH = $('header').height();
 			if ($(this).scrollTop() > topH) {
 				$('.btn-top').addClass('active');
 				console.log(topH)
@@ -68,150 +68,151 @@ jQuery(document).ready(function ($) {
 			menu.removeClass("active");
 			$("body").removeClass("fixed");
 		}
-	} 
+	}
 
 	$(window).resize(function () {
 		heightses();
-
+		initSwiper();
 	});
- 
+
 	heightses();
- 
+
 	// листалка по стр
 	$(" .scroll-link").click(function () {
-	       var elementClick = $(this).attr("href");
-	       var destination = $(elementClick).offset().top;
+		var elementClick = $(this).attr("href");
+		var destination = $(elementClick).offset().top;
 
-	           $('html, body').animate({ scrollTop: destination }, 1100);
+		$('html, body').animate({ scrollTop: destination }, 1100);
 
-	       return false;
-	   }); 
+		return false;
+	});
 	// slider
 	var swiper3 = new Swiper($('.s-prod__slider--js'), {
-		slidesPerView: 2, 
-		spaceBetween: 30,  
+		slidesPerView: 2,
+		spaceBetween: 30,
 		loop: true,
 		watchOverflow: true,
 		breakpointsInverse: true,
 		breakpoints: {
-		 
+
 			992: {
 				slidesPerView: 3
 			}
-		 
+
 		}
 	});
 	var slider = {
-		slidesPerView: 1, 
+		slidesPerView: 1,
 		spaceBetween: 0,
 		watchOverflow: true,
-			lazy: {
-				loadPrevNext: true,
-			},
-			effect: 'fade',
-			fadeEffect: {
-				crossFade: true
-			},
-			pagination: {
-				el: $('.header-block').find('.swiper-pagination'),
-				clickable: true,
-			},
-			on: {
+		lazy: {
+			loadPrevNext: true,
+		},
+		effect: 'fade',
+		fadeEffect: {
+			crossFade: true
+		},
+		pagination: {
+			el: $('.header-block').find('.swiper-pagination'),
+			clickable: true,
+		},
+		on: {
 
-				slideChange: function () {
-					setTimeout(function () {
-						swiperCounter()
-					 
-					}, 10);
+			slideChange: function () {
+				setTimeout(function () {
+					swiperCounter()
+
+				}, 10);
+			},
+
+		},
+
+		navigation: {
+			nextEl: $('.header-block').find('.swiper-button-next'),
+			prevEl: $('.header-block').find('.swiper-button-prev'),
+		},
+		loop: true,
+	}
+	var swiper2 = new Swiper($('.header-block').find('.slider--js'), slider);
+	// var swiper3 = new Swiper($(this).find('.slider--js2'), slider);
+
+	function swiperCounter() {
+		if ($(".swiper-counter")) {
+
+			$(".swiper-counter").text((swiper2.realIndex + 1) + ' / ' + (swiper2.slides.length - $('.header-block .swiper-slide-duplicate').length))
+			// console.log(swiper2.activeIndex)
+		}
+	}
+
+
+	var galleryThumbs = new Swiper('.gallery-thumbs', {
+		// spaceBetween: 10,
+		slidesPerView: 4,
+		loop: true,
+		spaceBetween: 10,
+		// freeMode: true,
+		// loopedSlides: 5, //looped slides should be the same
+		watchSlidesVisibility: true,
+		watchSlidesProgress: true,
+		loopedSlides: 5, //looped slides should be the same
+		direction: 'vertical',
+	});
+
+
+	var galleryTop = undefined;
+	function initSwiper() {
+		var screenWidth = $(window).width();
+		if (screenWidth < 1200 && galleryTop == undefined) {
+			var galleryTop = new Swiper('.gallery-top', {
+				spaceBetween: 10,
+				loop: true,
+				// navigation: {
+				//   nextEl: '.swiper-button-next',
+				//   prevEl: '.swiper-button-prev',
+				// },
+				thumbs: {
+					swiper: galleryThumbs,
 				},
+			});
+			// galleryTop.controller.control = galleryThumbs;
+			// galleryThumbs.controller.control = galleryTop;
 
-			},
-		 
-			navigation: {
-				nextEl: $('.header-block').find('.swiper-button-next'),
-				prevEl: $('.header-block').find('.swiper-button-prev'),
-			},
-			loop: true,
+		} else if (screenWidth > 1199 && galleryTop != undefined) {
+			galleryTop.destroy();
+			galleryTop = undefined;
+			jQuery('.gallery-top .swiper-wrapper').removeAttr('style');
+			jQuery('.gallery-top .swiper-slide').removeAttr('style');
 		}
-		var swiper2 = new Swiper($('.header-block').find('.slider--js'), slider);
-		// var swiper3 = new Swiper($(this).find('.slider--js2'), slider);
+	}
+	initSwiper();
+	var prodBodySl = new Swiper('.prod-body__slider--js', {
+		loop: true,
+		freeModeMomentum: true,
+		slidesPerView: 'auto',
+		// spaceBetween: 30,
+		freeMode: true,
+		watchOverflow: true,
+		navigation: {
+			nextEl: $('.prod-body').find('.swiper-button-next'),
+			prevEl: $('.prod-body').find('.swiper-button-prev'),
+		},
+		breakpoints: {
 
-		function swiperCounter() {
-				if($(".swiper-counter")) {
+			992: {
+				// slidesPerView: 3
+			}
 
-					$(".swiper-counter").text((swiper2.realIndex + 1) + ' / ' + (swiper2.slides.length  - $('.header-block .swiper-slide-duplicate').length) )
-					// console.log(swiper2.activeIndex)
-				}
-		} 
-
-		
-		var galleryThumbs = new Swiper('.gallery-thumbs', {
-      // spaceBetween: 10,
-      slidesPerView: 4,
-			loop: true,
-			spaceBetween: 10,
-      // freeMode: true,
-      // loopedSlides: 5, //looped slides should be the same
-      watchSlidesVisibility: true,
-      watchSlidesProgress: true,
-			loopedSlides: 5, //looped slides should be the same
-			direction: 'vertical',
-		});
-
-		
-		var galleryTop = undefined;
-function initSwiper() {
-	var screenWidth = $(window).width();
-    if(screenWidth < 1200 && galleryTop == undefined) {          
-    var galleryTop = new Swiper('.gallery-top', {
-      spaceBetween: 10,
-      loop:true,
-      // navigation: {
-      //   nextEl: '.swiper-button-next',
-      //   prevEl: '.swiper-button-prev',
-      // },
-      thumbs: {
-        swiper: galleryThumbs,
-      },
-		});
-		// galleryTop.controller.control = galleryThumbs;
-		// galleryThumbs.controller.control = galleryTop;
- 
-	} else if (screenWidth > 1199 && galleryTop != undefined) {
-		galleryTop.destroy();
-		galleryTop = undefined;
-		jQuery('.gallery-top .swiper-wrapper').removeAttr('style');
-		jQuery('.gallery-top .swiper-slide').removeAttr('style');            
-}     
-}
-var prodBodySl = new Swiper('.prod-body__slider--js', { 
-	loop: true,
-	freeModeMomentum: true,
-	slidesPerView: 'auto',
-	// spaceBetween: 30,
-	freeMode: true,
-	watchOverflow: true, 
-	navigation: {
-		nextEl: $('.prod-body').find('.swiper-button-next'),
-		prevEl: $('.prod-body').find('.swiper-button-prev'),
-	},
-	breakpoints: {
- 
-		992: {
-			// slidesPerView: 3
-		}
-	 
-	},
-});
-var wow = new WOW({
-	mobile: false
-});
-wow.init();
+		},
+	});
+	var wow = new WOW({
+		mobile: false
+	});
+	wow.init();
 });
 JSCCommon = {
 	// часть вызов скриптов здесь, для использования при AJAX
 	// функции для запуска lazy
-	 
+
 
 	// /LazyFunction
 
@@ -273,7 +274,7 @@ JSCCommon = {
 			$("body, html").toggleClass("fixed");
 			return false;
 		});
- 
+
 		$(".toggle-basket").click(function () {
 
 			// btnToggle.toggleClass("on");
@@ -282,7 +283,7 @@ JSCCommon = {
 			$("body, html").toggleClass("basket-fix");
 			return false;
 		});
- 
+
 	},
 	// /mobileMenu
 
@@ -303,23 +304,28 @@ JSCCommon = {
 		$('input[type="tel"]').attr("pattern", "[+]7 [(][0-9]{3}[)] [0-9]{3}-[0-9]{2}-[0-9]{2}").inputmask("+7 (999) 999-99-99");
 	},
 	// /inputMask
-	moreLine: function(){
+	moreLine: function () {
 		$('.text-more').moreLines({
-			linecount: 7,                  
+			linecount: 7,
 			buttontxtmore: "Подробнее",     	// Add your inner text for button
 			buttontxtless: "Скрыть",     	// Add your inner text for button
 			animationspeed: 250             	// Type your custom speed animation, by defaul is 'auto' auto = 1
 		});
 	},
-	zhowZoomIng: function(){
-		$(".zoom-img-js").each(function(){
+	zhowZoomIng: function () {
+		$(".zoom-img-js").each(function () {
 
 			new ImageZoom($(this), {
 				target: {
-				right: 25
-			}
+					right: 25
+				}
+			})
 		})
-	})
+		// 	$('.zoom-link').easyZoom({
+		// 		id: 'imagezoom',
+		// 		// preload: '<p class="preloader">Loading the image</p>',
+		// 		parent: '.prod-head__slide'
+		// });
 	}
 };
 
