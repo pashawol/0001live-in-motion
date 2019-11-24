9288643592c4d58cccb52e2f1e56b8ae7ae899ff
Pavel <wol1414@gmail.com>
2019-11-24 17:43:10 +0300

    edit catalog

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -5,14 +5,14 @@ module.exports = function (){
 	.pipe($.gp.sass().on("error", $.gp.notify.onError()))
 	
 	//.pipe(gulpif(envDev, sourcemaps.write({includeContent: false, sourceRoot: '/public'})))
-	.pipe($.gulpif(!$.envDev, $.gp.combineMq()))
+	.pipe($.gcmq())
 	.pipe($.gp.rename({suffix: '.min', prefix : ''}))
 	.pipe($.gp.autoprefixer({
 
 		grid: true,
-		overrideBrowserslist: ['last 10 versions']
+		overrideBrowserslist: ['last 5 versions']
 	}))
-	.pipe($.cleanCSS())
+	.pipe($.cleanCSS({compatibility: 'ie11'}))
 	.pipe($.tabify(2, true))
 	.pipe($.gulp.dest($.public + '/css'))
 	//.on('end', browserSync.stream());
